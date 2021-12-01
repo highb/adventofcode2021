1506e593539bf89ddbaca4580b8f3dfa92db999e
Brandon High <highb@users.noreply.github.com>
2021-12-01 15:23:20 -0800

    day1 part2: sliding window

--- advent-2021-day-1/src/main.rs
+++ advent-2021-day-1/src/main.rs
@@ -7,12 +7,17 @@ fn main() -> std::io::Result<()> {
     let mut puzzle_input = String::new();
     file.read_to_string(&mut puzzle_input)?;
 
-    println!("Puzzle Input:");
+    let mut measurements: Vec<i32> = Vec::new();
+    for input in puzzle_input.lines() {
+        measurements.push(i32::from_str(input).unwrap());
+    }
+
     let mut last = -1;
     let mut increasing = 0;
-    for input in puzzle_input.lines() {
-        let measurement: i64 = i64::from_str(input).unwrap();
+    for window in measurements.windows(3) {
+        println!("{:#?}", window);
         let mut change = "";
+        let measurement = window.iter().sum();
         if last < 0 {
             change = "N/A - no previous measurement";
         } else {
